afb2dab98564a59e4345c4a4e9018f378149f684
amiruddin1 <67670878+amiruddin1@users.noreply.github.com>
2023-06-28 21:04:28 +0530

    New Change

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -5,7 +5,7 @@ public class Calculator {
     public static void main(String[] args) throws InterruptedException {
         ChromeDriver cd = new ChromeDriver();
         cd.get("G:\\Study Material\\MCA-22MCA138\\Sem-2\\CA_870 SQA\\Assignment\\Leb Work\\SQA\\arthmetic.html");
-        cd.manage().window().maximize();
+        
         cd.findElement(By.name("first")).sendKeys("25");
         cd.findElement(By.name("second")).sendKeys("12");
         Thread.sleep(3000);
